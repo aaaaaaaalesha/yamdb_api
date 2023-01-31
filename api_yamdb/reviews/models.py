bac2d80f4e1bd484b7844817dcd715c8814dd7f5
Aleksey Aleksandrov <55093100+aaaaaaaalesha@users.noreply.github.com>
2023-01-31 23:30:52 +0300

    (feature:2) Genres, Categories, Titles models added

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+
+class Genres(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Слаг',
+    )
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Categories(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Слаг',
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Titles(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+    )
+    year = models.IntegerField(
+        verbose_name='Год выпуска',
+    )
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name='Описание',
+    )
+    genre = models.ManyToManyField(
+        Genres,
+        verbose_name='Жанр',
+    )
+    category = models.ForeignKey(
+        Categories,
+        on_delete=models.CASCADE,
+        related_name='category',
+    )
