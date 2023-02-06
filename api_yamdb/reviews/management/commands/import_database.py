132c55f46d6843ada15f6fcf6b5eaf6298e930a7
Aleksey Aleksandrov <55093100+aaaaaaaalesha@users.noreply.github.com>
2023-02-06 20:36:18 +0300

    (feature 2): all tests have passed

--- api_yamdb/reviews/management/commands/import_database.py
+++ api_yamdb/reviews/management/commands/import_database.py
@@ -14,6 +14,66 @@ from ...models import (
 )
 
 
+def _add_genres(row):
+    Genre.objects.get_or_create(
+        id=row['id'],
+        name=row['name'],
+        slug=row['slug'],
+    )
+
+
+def _add_categories(row):
+    Category.objects.get_or_create(
+        id=row['id'],
+        name=row['name'],
+        slug=row['slug'],
+    )
+
+
+def _add_titles(row):
+    Title.objects.get_or_create(
+        id=row['id'],
+        name=row['name'],
+        year=row['year'],
+        category=row['category_id'],
+    )
+
+
+def _add_users(row):
+    User.objects.get_or_create(
+        id=row['id'],
+        username=row['username'],
+        email=row['email'],
+        role=row['role'],
+        bio=row['bio'],
+        first_name=row['first_name'],
+        last_name=row['last_name'],
+    )
+
+
+def _add_reviews(row):
+    title, _ = Title.objects.get_or_create(id=row['title_id'])
+    Review.objects.get_or_create(
+        id=row['id'],
+        title=title,
+        text=row['text'],
+        author=row['author'],
+        score=row['score'],
+        pub_date=row['pub_date']
+    )
+
+
+def _add_comments(row):
+    review, _ = Review.objects.get_or_create(id=row['review_id'])
+    Comment.objects.get_or_create(
+        id=row['id'],
+        author=row['author'],
+        review=review,
+        text=row['text'],
+        pub_date=row['pub_date'],
+    )
+
+
 class Command(BaseCommand):
     """ImportDatabaseFromCSVCommand"""
     help = 'Imports database from csv by passed path.'
@@ -23,13 +83,14 @@ class Command(BaseCommand):
         'data',
     )
 
-    # CSV_MODELS = {
-    #     'genre.csv': Genre,
-    #     'category.csv': Category,
-    #     'titles.csv': Title,
-    #     'users.csv': User,
-    #
-    # }
+    CSV_MODELS = {
+        'genre.csv': _add_genres,
+        'category.csv': _add_categories,
+        'titles.csv': _add_titles,
+        'users.csv': _add_users,
+        'review.csv': _add_reviews,
+        'comments.csv': _add_comments,
+    }
 
     def __process_path(self, path: str) -> None:
         if not os.path.exists(path):
@@ -57,11 +118,19 @@ class Command(BaseCommand):
             self.import_csv(file)
 
     def import_csv(self, csv_file_path: str) -> None:
+        basename = os.path.basename(csv_file_path)
+        procedure = self.CSV_MODELS.get(basename)
+        if procedure is None:
+            self.stderr(
+                f'Basename of {csv_file_path} not '
+                f'matches any of {self.CSV_MODELS.keys()}'
+            )
+            return
+
         with open(csv_file_path, encoding='utf-8', mode='r') as csv_file:
             dict_reader = csv.DictReader(csv_file)
-            # fields = dict_reader.fieldnames
-            # print(dict_reader)
-            # print(fields)
+            for row in dict_reader:
+                procedure(row)
 
     def handle(self, *paths, **options):
         if not paths:
